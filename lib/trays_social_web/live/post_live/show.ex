2054d7863e9279d896be766ea1decf8d3a45bd2b
Joseph Morgan <joser.morgan6@gmail.com>
2026-02-06 18:09:56 -0500

    displaying full post with large photo, complete ingredient list, tools, cooking steps, tags, and delete button for owner only.

--- lib/trays_social_web/live/post_live/show.ex
+++ lib/trays_social_web/live/post_live/show.ex
@@ -16,4 +16,30 @@ defmodule TraysSocialWeb.PostLive.Show do
 
     {:ok, socket}
   end
+
+  @impl true
+  def handle_event("delete", _params, socket) do
+    post = socket.assigns.post
+
+    # Verify user is the owner
+    if socket.assigns.current_scope && socket.assigns.current_scope.user &&
+         socket.assigns.current_scope.user.id == post.user_id do
+      case Posts.delete_post(post) do
+        {:ok, _post} ->
+          {:noreply,
+           socket
+           |> put_flash(:info, "Post deleted successfully")
+           |> push_navigate(to: ~p"/")}
+
+        {:error, _changeset} ->
+          {:noreply,
+           socket
+           |> put_flash(:error, "Failed to delete post")}
+      end
+    else
+      {:noreply,
+       socket
+       |> put_flash(:error, "You are not authorized to delete this post")}
+    end
+  end
 end
---
+++ test/trays_social_web/live/post_live/show_test.exs
@@ -0,0 +1,79 @@
+defmodule TraysSocialWeb.PostLive.ShowTest do
+  use TraysSocialWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+  import TraysSocial.PostsFixtures
+  import TraysSocial.AccountsFixtures
+
+  describe "Show" do
+    test "displays post with all details", %{conn: conn} do
+      user = user_fixture()
+      post = post_fixture(user_id: user.id)
+
+      {:ok, _view, html} = live(conn, ~p"/posts/#{post.id}")
+
+      assert html =~ post.user.username
+      assert html =~ post.caption
+      assert html =~ "#{post.cooking_time_minutes} minutes"
+      assert html =~ post.photo_url
+    end
+
+    test "shows delete button for post owner", %{conn: conn} do
+      user = user_fixture()
+      post = post_fixture(user_id: user.id)
+
+      {:ok, _view, html} =
+        conn
+        |> log_in_user(user)
+        |> live(~p"/posts/#{post.id}")
+
+      assert html =~ "Delete Post"
+    end
+
+    test "does not show delete button for non-owner", %{conn: conn} do
+      owner = user_fixture()
+      other_user = user_fixture()
+      post = post_fixture(user_id: owner.id)
+
+      {:ok, _view, html} =
+        conn
+        |> log_in_user(other_user)
+        |> live(~p"/posts/#{post.id}")
+
+      refute html =~ "Delete Post"
+    end
+
+    test "deletes post when owner clicks delete button", %{conn: conn} do
+      user = user_fixture()
+      post = post_fixture(user_id: user.id)
+
+      {:ok, view, _html} =
+        conn
+        |> log_in_user(user)
+        |> live(~p"/posts/#{post.id}")
+
+      assert view
+             |> element("button", "Delete Post")
+             |> render_click()
+
+      assert_redirect(view, ~p"/")
+
+      # Verify post was soft deleted
+      deleted_post = TraysSocial.Repo.get(TraysSocial.Posts.Post, post.id)
+      assert deleted_post.deleted_at != nil
+    end
+
+    test "back to feed link works", %{conn: conn} do
+      user = user_fixture()
+      post = post_fixture(user_id: user.id)
+
+      {:ok, view, _html} = live(conn, ~p"/posts/#{post.id}")
+
+      assert view
+             |> element("a", "Back to feed")
+             |> render_click()
+
+      assert_redirect(view, ~p"/")
+    end
+  end
+end
